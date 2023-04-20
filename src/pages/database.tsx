9ece3c301b589595727c69743cec7d5cb6313d1e
Tom Lawless <lawlessptom@gmail.com>
2023-04-20 13:59:18 -0400

    initial dev commit

---
+++ src/pages/database.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+
+
+const Database: NextPage = () => {
+
+    return(
+        <div><h1>Hi</h1></div>
+    )
+}
+export default Database;
